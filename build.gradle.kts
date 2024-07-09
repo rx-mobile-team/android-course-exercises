61ac0a25318915f1f110e287dd673d5b9d7bf5f7
hoc081098@gmail.com <hoc081098@gmail.com>
2024-07-09 15:49:56 +0700

    kotlin 2.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,8 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
 
 plugins {
-  kotlin("jvm") version "1.9.23"
+  kotlin("jvm") version "2.0.0"
 }
 
 group = "com.rxmobileteam"
@@ -15,18 +16,27 @@ dependencies {
   testImplementation(kotlin("test"))
   implementation("org.jetbrains:annotations:24.0.1")
   implementation("io.reactivex.rxjava3:rxjava:3.1.8")
-  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0-RC")
+}
+
+kotlin {
+  jvmToolchain {
+    languageVersion.set(JavaLanguageVersion.of(19))
+    vendor.set(JvmVendorSpec.AZUL)
+  }
 }
 
 tasks.test {
   useJUnitPlatform()
 }
 
-tasks.withType<KotlinCompile> {
-  kotlinOptions.jvmTarget = JavaVersion.VERSION_13.toString()
+tasks.withType<KotlinJvmCompile> {
+  compilerOptions {
+    jvmTarget = JvmTarget.JVM_19
+  }
 }
 
 java {
-  sourceCompatibility = JavaVersion.VERSION_13
-  targetCompatibility = JavaVersion.VERSION_13
+  sourceCompatibility = JavaVersion.VERSION_19
+  targetCompatibility = JavaVersion.VERSION_19
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,5 @@
+plugins {
+  id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+}
 
-rootProject.name = "android003_exercises"
-
+rootProject.name = "rxmobileteam_android_exercises"
